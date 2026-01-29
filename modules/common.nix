65ce46f74f3f3a860b10040badd262f01e61677c
Leon Hernandez <48255337+lnhrnndz@users.noreply.github.com>
2026-01-29 19:25:14 +0100

    Start fish if not already fish

--- modules/common.nix
+++ modules/common.nix
@@ -54,6 +54,17 @@
 
   programs.zsh.enable = true;
 
+  # launch fish unless the parent process is already fish
+  programs.bash = {
+  interactiveShellInit = ''
+    if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+      then
+        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+      fi
+    '';
+  };
+
   services.openssh.enable = true;
   #services.tailscale.enable = true;
 
